6b2ec1c9975c4f8e17686407c7201d9d30d05117
akshat-shah <akshat.shah3001@gmail.com>
2016-10-20 17:10:10 -0400

    Use Case 3 - work in progress

--- bot/envBot.js
+++ bot/envBot.js
@@ -6,8 +6,8 @@ var dockerData = require('./dockerData');
 
 var messageTypes = ['direct_message','direct_mention','mention'];
 
-var gitSite = /(http|ftp|https):\/\/github([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/;
-var urlPattern = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/;
+var gitSite = /(http|ftp|https):\/\/(github+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/;
+var urlPattern = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))+([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/;
 var starterREGEX = [/hey/i, /help/i, /how/i];
 var dockerCmdREGEX = [/request docker image( |:)*(python|mean|lamp)+/i];
 var dockerFileREGEX = [/(docker file|dockerfile)/i, gitSite];
@@ -49,8 +49,10 @@ controller.hears(dockerFileREGEX, messageTypes, function(bot, message) {
 
 		var repoData = {
 			body: message.text,
-			link: pattern.exec(message.text)[0]
+			link: urlPattern.exec(message.text)[0]
 		};
+
+		console.log('Calling create docker file', repoData.link);
 
 		createDockerFile(repoData.link, function(dockerFile) {
 			if (dockerFile) {
@@ -95,9 +97,59 @@ controller.hears(dockerCmdREGEX, messageTypes, function(bot, message) {
 });
 
 // TODO USE CASE 3 : Build Docker image from Github repo
+// Still in progress.
+controller.hears('use case 3', messageTypes, function(bot, message) {
+	// if(!urlPattern.test(message.text)) {
+	// 	bot.reply(message, '<@' + message.user + '>, Can I have the URL?');
+	// } else if(urlPattern.test(message.text) && !gitSite.test(message.text)) {
+	// 	bot.reply(message, '<@' + message.user + '>, Can I have a valid URL(i.e., belonging to a GitHub repository)?');
+	// } else {
+
+		var repoData = {
+			body: message.text,
+			link: 'https://github.com/alt-code/DockerizeMe.git'
+		};
+
+		console.log('Calling create docker file', repoData.link);
+
+		createDockerFile(repoData.link, function(dockerFile) {
+			if (dockerFile) {
+				
+				
+				if (repoData.link.charAt(repoData.link.length-1) === '/') {
+					repoData.link = repoData.link.substring(0, repoData.link.length-1);
+				}
+
+				var repoName = repoData.link.substring(repoData.link.lastIndexOf('/')+1);
+				repoName = repoName.replace('.git', '');
+
+				createDockerImage(repoName, function(err, data) {
+					if (err) {
+						console.log('Error while creating docker Image : ' + err);
+					} else {
+						var command = 'docker pull ashah7/' + repoName.toLowerCase;
+						var reply = {
+							"attachments": [{
+								"title" : "Commands to pull Docker Image",
+								"pretext" : "",
+								"text" : command,
+								"mrkdwn_in" : ["text", "pretext"]
+						}]};
+
+						bot.reply(message, reply);
+					}
+				});
+
+
+			} else {
+				return bot.reply(message, 'Error in creating docker file.')
+			}
+		});
+	//}
+});
 
 function createDockerFile(repo, callback) {
-	
+	console.log('calling bash script');
 	const exec = require('child_process').exec;
 
 	exec(`sh create_dockerfile.sh ${repo}`, {cwd : constants.cwd}, (error, stdout, stderr) => {
@@ -106,15 +158,28 @@ function createDockerFile(repo, callback) {
 		} else if (stderr) {
 			console.log(`I/O Standard Error  : ${stderr}`);
 		} else {
-			exec('cat DockerFile', {cwd : constants.cwd}, (error, stdout, stderr) => {
-				if (error) {
-					console.log(`Error in executing command : ${error}`);
-				} else if (stderr) {
-					console.log(`I/O Standard Error  : ${stderr}`);
-				} else {
-					return callback(stdout);
-				}
-			});
+			console.log('return answer', stdout);
+			callback(stdout);
+		}
+	});
+}
+
+function createDockerImage(repoName, callback) {
+	
+	var cwd = './DockerizeMe/' + repoName + '/';
+
+	console.log('Calling dockerize.sh');
+
+	const exec = require('child_process').exec;
+	exec(`sh dockerize.sh ${repoName.toLowerCase()}`, {cwd : cwd}, (error, stdout, stderr) => {
+		if (error) {
+			console.log(`Error in executing command : ${error}`);
+			callback(error, stdout);
+		} else if (stderr) {
+			console.log(`I/O Standard Error  : ${stderr}`);
+			callback(stderr, stdout);
+		} else {
+			callback(null, stdout);
 		}
 	});
 }
